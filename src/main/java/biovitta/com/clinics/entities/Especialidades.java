c91a001ab8f42f3d935f2ca751908f1ce828790e
Thiago P <thiago.p.sampaio07@gmail.com>
2025-06-01 15:47:13 -0300

    :sparkles: feat: Implementando a entidade Especialidades

---
+++ src/main/java/biovitta/com/clinics/entities/Especialidades.java
@@ -0,0 +1,38 @@
+package biovitta.com.clinics.entities;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table (name =  "tb_especialidade")
+public class Especialidades {
+
+    @Id @GeneratedValue(strategy =  GenerationType.IDENTITY)
+    @Column( name = "especialidade_id", unique = true)
+    private Long especialidadeId;
+    @Column( name = "nome", unique = true)
+    private String nome;
+
+    public Especialidades() {
+    }
+
+    public Especialidades(Long especialidadeId, String nome) {
+        this.especialidadeId = especialidadeId;
+        this.nome = nome;
+    }
+
+    public Long getEspecialidadeId() {
+        return especialidadeId;
+    }
+
+    public void setEspecialidadeId(Long especialidadeId) {
+        this.especialidadeId = especialidadeId;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+}
