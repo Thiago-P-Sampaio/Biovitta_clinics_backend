eae3b2e3b6ff648919a1897ef3f5fedb34630929
Thiago P <thiago.p.sampaio07@gmail.com>
2025-06-03 16:37:51 -0300

    :construction: Implementando Métodos restantes | Médicos | Paciente

--- src/main/java/biovitta/com/clinics/controllers/UsuarioController.java
+++ src/main/java/biovitta/com/clinics/controllers/UsuarioController.java
@@ -6,6 +6,8 @@ import biovitta.com.clinics.DTOs.UsuarioDTO;
 import biovitta.com.clinics.DTOs.cadastro.MedicoRequestDTO;
 import biovitta.com.clinics.DTOs.cadastro.PacienteRequestDTO;
 import biovitta.com.clinics.entities.Paciente;
+import biovitta.com.clinics.repositories.MedicoRepositorio;
+import biovitta.com.clinics.services.MedicoService;
 import biovitta.com.clinics.services.PacienteService;
 import biovitta.com.clinics.services.UsuarioService;
 import jakarta.validation.Valid;
@@ -24,6 +26,9 @@ public class UsuarioController {
 
     @Autowired
     PacienteService pacienteService;
+
+    @Autowired
+    MedicoService medicoService;
 
 
         @PostMapping("/paciente/add")
@@ -54,5 +59,30 @@ public class UsuarioController {
     @DeleteMapping("paciente/dell/{id}")
     public  String deletarPaciente(@PathVariable Long id){
             return pacienteService.deletarPaciente(id);
+    }
+
+    @PutMapping("paciente/edit/{id}")
+    public  ResponseEntity<PacienteDTO>  editarPaciente(@PathVariable Long id, @RequestBody PacienteRequestDTO dto){
+            return ResponseEntity.ok(pacienteService.editarPaciente(dto, id));
+    }
+
+    @GetMapping("medico/get/all")
+    public  ResponseEntity<List<MedicoDTO>> listarMedicos(){
+            return ResponseEntity.ok(medicoService.listarMedicos());
+    }
+
+    @GetMapping("medico/get/{crm}")
+    public ResponseEntity<MedicoDTO> buscarMedico(@PathVariable String crm){
+            return ResponseEntity.ok(medicoService.buscarMedico(crm));
+    }
+
+    @DeleteMapping("medico/dell/{crm}")
+    public String deletarMedico(@PathVariable String crm){
+            return medicoService.deletarMedico(crm);
+    }
+
+    @PutMapping("medico/edit/{crm}")
+    public ResponseEntity<MedicoDTO> editarMedico(@PathVariable String crm, @RequestBody MedicoRequestDTO dto){
+            return ResponseEntity.ok(medicoService.editarMedico(dto, crm));
     }
 }
