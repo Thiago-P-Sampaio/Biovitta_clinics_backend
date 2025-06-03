c52988ac9b4b1b4787f5eb6d16cbf19354794e51
Thiago P <thiago.p.sampaio07@gmail.com>
2025-06-02 21:51:01 -0300

    :sparkles: DTO para receber as informações de ACESSO

---
+++ src/main/java/biovitta/com/clinics/DTOs/login/RespostaAutenticacaoDTO.java
@@ -0,0 +1,35 @@
+package biovitta.com.clinics.DTOs.login;
+
+import biovitta.com.clinics.entities.Permissao;
+import biovitta.com.clinics.entities.Usuario;
+
+public class RespostaAutenticacaoDTO {
+  //Classe para retornar o token de acesso ao sistema
+    private String token;
+    private String role;
+    private String usuario;
+
+
+    public RespostaAutenticacaoDTO(String token, Usuario entity) {
+        this.token = token;
+        this.role = entity.getRole().toString()  ;
+        this.usuario = entity.getUsuario() ;
+    }
+
+    // Para definir o TOKEN!
+    public void setToken(String token) {
+        this.token = token;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public String getUsuario() {
+        return usuario;
+    }
+}
